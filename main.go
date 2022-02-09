4012c279bb85e6793e02e3504c9fcecc7451287d
Alexander G <alexander.maverick1996@gmail.com>
2022-02-09 10:26:18 -0300

    conditions added

--- main.go
+++ main.go
@@ -3,14 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	valor1 := 1
-	// valor2 := 2
+	valor1 := 2
+	valor2 := 2
 
 	if valor1 == 1 {
 		fmt.Println("Es 1")
 
 	} else {
 		fmt.Println("No es 1")
+	}
 
+	//with and
+
+	if valor1 == 1 && valor2 == 3 {
+
+		fmt.Println("I'ts true")
+
+	} else {
+		fmt.Println("false")
+	}
+	//with or
+
+	if valor1 == 1 || valor2 == 3 {
+
+		fmt.Println("I'ts true")
+
+	}
+
+	if valor1%2 == 0 {
+		fmt.Println("es par")
+	} else {
+		fmt.Println("no es par")
 	}
 }
