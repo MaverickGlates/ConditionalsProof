e7a181802238f1385fd46fa434f1715ba21a4738
Alexander G <alexander.maverick1996@gmail.com>
2022-02-09 11:49:25 -0300

    working with conditions, kewywords and arrays/slice

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func main() {
 	} else {
 		fmt.Println("false")
 	}
+
 	//with or
 
 	if valor1 == 1 || valor2 == 3 {
@@ -30,9 +31,88 @@ func main() {
 
 	}
 
+	// recognize if a number is even
+
 	if valor1%2 == 0 {
 		fmt.Println("es par")
 	} else {
 		fmt.Println("no es par")
 	}
+
+	//types module declaration
+
+	modulo := 3 % 2
+	switch modulo {
+	case 0:
+		fmt.Println("es par")
+
+	default:
+		fmt.Println("no es par")
+	}
+
+	switch module := 4 % 2; module {
+	case 0:
+		fmt.Println("es par")
+
+	default:
+		fmt.Println("no es par")
+	}
+
+	//module without condition
+	value := 200
+	switch {
+	case value > 100:
+		fmt.Println("is greater than 100")
+	case value < 0:
+		fmt.Println(" is lesser than  0")
+	default:
+		fmt.Println(" no conditon")
+	}
+
+	//Keywords
+
+	//Defer
+	// defer fmt.Println("Hola")
+	// fmt.Println("Mundo")
+
+	//Continue & Break
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+
+		//continue
+		if i == 2 {
+			fmt.Println("Es 2")
+			continue
+		}
+
+		// Break
+		if i == 8 {
+			fmt.Println("Break")
+			break
+		}
+	}
+
+	// Array
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
+
+	// Slice, similar declaration to array.
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
+
+	// In slice method
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+	// Append
+	slice = append(slice, 7)
+	fmt.Println(slice[7])
+
+	// Append new list
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 }
